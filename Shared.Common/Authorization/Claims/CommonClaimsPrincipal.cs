9ab4a8961a09b8fa16e61cdb5935d6c9114e846e
Islom Gulomkodirov <gicorp.demo@gmail.com>
2024-04-18 22:01:08 +0500

    add: baseAuthController and commonClaimsPrinciple to handle JWT claims

--- Shared.Common/Authorization/Claims/CommonClaimsPrincipal.cs
+++ Shared.Common/Authorization/Claims/CommonClaimsPrincipal.cs
@@ -1,6 +1,27 @@
-﻿namespace Shared.Common.Authorization.Claims;
+﻿using System.Security.Claims;
+using System.Security.Principal;
 
-public class CommonClaimsPrincipal
+namespace Shared.Common.Authorization.Claims;
+
+public class CommonClaimsPrincipal : ClaimsPrincipal
 {
-    
+    private readonly ClaimsIdentity? _claimsIdentity;
+
+    public CommonClaimsPrincipal(IPrincipal principal) : base(principal)
+    {
+        _claimsIdentity = (principal as ClaimsPrincipal)?.Identity as ClaimsIdentity;
+    }
+
+    private string GetClaimValue(string claimType) => _claimsIdentity?.FindFirst(claimType)?.Value!;
+
+    public Guid? UserId => TryParseGuid(GetClaimValue(Helpers.Constants.JwtClaimIdentifiers.Id));
+    public string LanguageId => GetClaimValue(Helpers.Constants.JwtClaimIdentifiers.LanguageId);
+    public string UserName => GetClaimValue(Helpers.Constants.JwtClaimIdentifiers.UserName);
+    public string PhoneNumber => GetClaimValue(Helpers.Constants.JwtClaimIdentifiers.PhoneNumber);
+    public Guid? SessionId => TryParseGuid(GetClaimValue(Helpers.Constants.JwtClaimIdentifiers.SessionId));
+
+    private Guid? TryParseGuid(string value)
+    {
+        return Guid.TryParse(value, out Guid result) ? result : null;
+    }
 }
--- Shared.Common/Controllers/BaseAuthController.cs
+++ Shared.Common/Controllers/BaseAuthController.cs
@@ -1,6 +1,22 @@
-﻿namespace Shared.Common.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Common.Authorization.Claims;
 
-public class BaseAuthController
+namespace Shared.Common.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+public class BaseAuthController : ControllerBase
 {
-    
+    private CommonClaimsPrincipal _commonClaimsPrincipal;
+
+    public new CommonClaimsPrincipal User
+    {
+        get
+        {
+            if (_commonClaimsPrincipal != null)
+                return _commonClaimsPrincipal;
+
+            return new CommonClaimsPrincipal(HttpContext.User);
+        }
+    }
 }
