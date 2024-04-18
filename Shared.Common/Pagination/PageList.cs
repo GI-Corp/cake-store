f96f50b2ddf643bc4bb405a0b735df2729957a89
Islom Gulomkodirov <gicorp.demo@gmail.com>
2024-04-18 22:04:28 +0500

    add: abstract pagination classes with list and grid resulting response

--- Shared.Common/Pagination/PageList.cs
+++ Shared.Common/Pagination/PageList.cs
@@ -1,6 +1,24 @@
 ﻿namespace Shared.Common.Pagination;
 
-public class PageList
+public class PagedList<T> : List<T>
 {
-    
+    public PagedList(IEnumerable<T> items, int pageNumber, int pageSize)
+    {
+        IEnumerable<T> enumerable = items.ToList();
+        var count = enumerable.Count();
+
+        TotalCount = count;
+        PageSize = pageSize;
+        CurrentPage = pageNumber;
+        TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+        AddRange(enumerable);
+    }
+
+    public int CurrentPage { get; }
+    public int TotalPages { get; }
+    public int PageSize { get; private set; }
+    public int TotalCount { get; private set; }
+    public bool HasPrevious => CurrentPage > 1;
+    public bool HasNext => CurrentPage < TotalPages;
 }
