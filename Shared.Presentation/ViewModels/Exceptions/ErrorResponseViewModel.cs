4e983630c7e36b2ab228c065d2305754738c06cc
Islom Gulomkodirov <gicorp.demo@gmail.com>
2024-04-18 22:02:10 +0500

    add: standard error response view model with custom fields

--- Shared.Presentation/ViewModels/Exceptions/ErrorResponseViewModel.cs
+++ Shared.Presentation/ViewModels/Exceptions/ErrorResponseViewModel.cs
@@ -2,5 +2,23 @@
 
 public class ErrorResponseViewModel
 {
-    
+    /// <summary>
+    ///     Gets or sets error code.
+    /// </summary>
+    public short Code { get; set; }
+
+    /// <summary>
+    ///     Gets or sets languageId.
+    /// </summary>
+    public string LanguageId { get; set; }
+
+    /// <summary>
+    ///     Gets or sets error details.
+    /// </summary>
+    public string Details { get; set; }
+
+    /// <summary>
+    ///     Gets or sets timestamp.
+    /// </summary>
+    public DateTime TimeStamp { get; set; } = DateTime.UtcNow;
 }
