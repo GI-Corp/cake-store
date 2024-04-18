c2d44a15e68fa3620fae390633d681cbe5a75833
Islom Gulomkodirov <gicorp.demo@gmail.com>
2024-04-18 22:04:01 +0500

    add: system and request static class extensions

--- Shared.Common/Extensions/Request/HeaderExtensions.cs
+++ Shared.Common/Extensions/Request/HeaderExtensions.cs
@@ -1,6 +1,12 @@
-﻿namespace Shared.Common.Extensions.Request;
+﻿using Shared.Common.Authorization.Claims;
+using Shared.Common.Helpers;
 
-public class HeaderExtensions
+namespace Shared.Common.Extensions.Request;
+
+public static class HeaderExtensions
 {
-    
+    public static string GetDefaultLanguageId(CommonClaimsPrincipal claims)
+    {
+        return claims.LanguageId;
+    }
 }
