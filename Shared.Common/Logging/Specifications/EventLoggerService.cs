3a04e48def3d187c8f0fa3120d26609e95a352fa
Islom Gulomkodirov <gicorp.demo@gmail.com>
2024-04-18 22:01:45 +0500

    add: eventLoggerService specification with methods from its interface

--- Shared.Common/Logging/Specifications/EventLoggerService.cs
+++ Shared.Common/Logging/Specifications/EventLoggerService.cs
@@ -1,6 +1,51 @@
-﻿namespace Shared.Common.Logging.Specifications;
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Logging;
+using Shared.Common.Logging.Interfaces;
 
-public class EventLoggerService
+namespace Shared.Common.Logging.Specifications;
+
+public class EventLoggerService<T> : IEventLoggerService<T>
 {
-    
+    private readonly ILogger<T> _logger;
+
+    public EventLoggerService(ILogger<T> logger)
+    {
+        _logger = logger;
+    }
+
+    public void LogError(string message)
+    {
+        _logger.LogError(message);
+        LogEvent(message, "ERROR");
+    }
+
+    public void LogInformation(string message)
+    {
+        _logger.LogInformation(message);
+        LogEvent(message);
+    }
+
+    public void LogCritical(string message)
+    {
+        _logger.LogCritical(message);
+        LogEvent(message, "CRITICAL");
+    }
+
+    public void LogTrace(string message)
+    {
+        _logger.LogTrace(message);
+        LogEvent(message, "TRACE");
+    }
+
+    public void LogWarning(string message)
+    {
+        _logger.LogWarning(message);
+        LogEvent(message, "WARNING");
+    }
+
+    protected void LogEvent(string message, string logLevel = "INFO")
+    {
+        var currentActivity = Activity.Current;
+        currentActivity?.AddEvent(new ActivityEvent($"{logLevel} {nameof(T)}: {message}"));
+    }
 }
