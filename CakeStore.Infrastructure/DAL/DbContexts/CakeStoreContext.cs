95415fb4038ed6045fd65d1ad57a326910bf767f
Islom Gulomkodirov <gi_account@mail.ru>
2024-04-21 23:14:18 +0500

    add: cakeStore and reference contexts

--- CakeStore.Infrastructure/DAL/DbContexts/CakeStoreContext.cs
+++ CakeStore.Infrastructure/DAL/DbContexts/CakeStoreContext.cs
@@ -1,6 +1,20 @@
+using CakeStore.Domain.Entities.CakeStore;
+using Microsoft.EntityFrameworkCore;
+
 namespace CakeStore.Infrastructure.DAL.DbContexts;
 
-public class CakeStoreContext
+public class CakeStoreContext : DbContext
 {
+    private const string CakeStoreScheme = "CakeStore";
+
+    public CakeStoreContext(DbContextOptions<CakeStoreContext> contextOptions) : base(contextOptions)
+    {
+    }
     
+    public DbSet<Error> Errors { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        
+    }
 }
--- CakeStore.Infrastructure/DAL/DbContexts/ReferenceContext.cs
+++ CakeStore.Infrastructure/DAL/DbContexts/ReferenceContext.cs
@@ -1,14 +1,17 @@
+using CakeStore.Domain.Entities.Reference;
 using Microsoft.EntityFrameworkCore;
 
 namespace CakeStore.Infrastructure.DAL.DbContexts;
 
-public class CakeStoreContext : DbContext
+public class ReferenceContext : DbContext
 {
-    private const string CakeStoreScheme = "CakeStore";
+    private const string ReferenceScheme = "References";
 
-    public CakeStoreContext(DbContextOptions<CakeStoreContext> contextOptions) : base(contextOptions)
+    public ReferenceContext(DbContextOptions<ReferenceContext> contextOptions) : base(contextOptions)
     {
     }
+    
+    public DbSet<Language> Languages { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
