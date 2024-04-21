4309e33a117e39b3d8bf3e07bf128bfbc08b4341
Islom Gulomkodirov <gi_account@mail.ru>
2024-04-21 23:12:00 +0500

    update: json converter module to handle json objects

--- Shared.Common/Helpers/DecimalJsonConverter.cs
+++ Shared.Common/Helpers/DecimalJsonConverter.cs
@@ -1,6 +1,35 @@
+using System.Globalization;
+using Newtonsoft.Json;
+
 namespace Shared.Common.Helpers;
 
-public class DecimalJsonConverter
+public class DecimalJsonConverter : JsonConverter
 {
-    
+    public override bool CanConvert(Type objectType)
+    {
+        return objectType == typeof(decimal);
+    }
+
+    public override object ReadJson(JsonReader reader, Type objectType, object existingValue,
+        JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.String)
+        {
+            if ((string)reader.Value == string.Empty)
+            {
+                return 0;
+            }
+        }
+        else if (reader.TokenType is JsonToken.Float or JsonToken.Integer)
+        {
+            return Convert.ToDecimal(reader.Value);
+        }
+
+        throw new JsonSerializationException("Unexpected token type: " + reader.TokenType);
+    }
+
+    public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+    {
+        writer.WriteRawValue(((decimal)value).ToString("F2", CultureInfo.InvariantCulture));
+    }
 }
