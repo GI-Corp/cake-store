692bbd0260a1d6acd957c8a30fa32c347a757bdb
Islom Gulomkodirov <gi_account@mail.ru>
2024-04-21 23:13:09 +0500

    add: abstract reference entity base class

--- Shared.Domain/Entities/Abstraction/ProtectedReferenceEntityBase.cs
+++ Shared.Domain/Entities/Abstraction/ProtectedReferenceEntityBase.cs
@@ -1,6 +1,19 @@
 namespace Shared.Domain.Entities.Abstraction;
 
-public class ProtectedReferenceEntityBase
+public abstract class ProtectedReferenceEntityBase<TKey>
 {
-    
+    /// <summary>
+    ///  Gets or sets unique identifier.
+    /// </summary>
+    public TKey Id { get; private set; }
+
+    /// <summary>
+    ///  Gets or sets name.
+    /// </summary>
+    public string Name { get; private set; }
+
+    /// <summary>
+    ///  Gets or sets display name.
+    /// </summary>
+    public string DisplayName { get; private set; }
 }
