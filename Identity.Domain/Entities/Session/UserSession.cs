32eab0a64051500c8c6e2013e92b42ae55c75347
Islom Gulomkodirov <gicorp.demo@gmail.com>
2024-04-22 20:41:12 +0500

    add: UserSession and RefreshToken entities to Identity.Domain

--- Identity.Domain/Entities/Session/UserSession.cs
+++ Identity.Domain/Entities/Session/UserSession.cs
@@ -1,6 +1,17 @@
-﻿namespace Identity.Domain.Entities.Session;
+﻿using Identity.Domain.Entities.Auth;
+using Shared.Domain.Entities.Abstraction;
 
-public class UserSession
+namespace Identity.Domain.Entities.Session;
+
+public class UserSession : IBaseEntity<Guid>
 {
-    
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public AppUser AppUser { get; set; }
+    public string? HostAddress { get; set; }
+    public short StatusId { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
 }
--- Identity.Domain/Entities/Token/RefreshToken.cs
+++ Identity.Domain/Entities/Token/RefreshToken.cs
@@ -1,6 +1,20 @@
-﻿namespace Identity.Domain.Entities.Token;
+﻿using Identity.Domain.Entities.Auth;
+using Identity.Domain.Entities.Session;
+using Shared.Domain.Entities.Abstraction;
 
-public class RefreshToken
+namespace Identity.Domain.Entities.Token;
+
+public class RefreshToken : IBaseEntity<Guid>
 {
-    
+    public Guid Id { get; set; }
+    public string Token { get; set; }
+    public DateTime ExpiresAt { get; set; }
+    public Guid UserId { get; set; }
+    public AppUser AppUser { get; set; }
+    public Guid SessionId { get; set; }
+    public UserSession UserSession { get; set; }
+    public bool IsActive { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public DateTime? DeletedAt { get; set; }
 }
