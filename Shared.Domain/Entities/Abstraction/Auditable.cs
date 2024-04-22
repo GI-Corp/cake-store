b65d98c7bc5b6a395425d53fff8f5568ed43392c
Islom Gulomkodirov <gicorp.demo@gmail.com>
2024-04-22 20:08:35 +0500

    remove: unused folders and directories, update abstract entities in shared module

--- Shared.Domain/Entities/Abstraction/Auditable.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shared.Domain.Entities.Abstraction;
-
-public abstract class Auditable
-{
-    public virtual DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public virtual DateTime? UpdatedAt { get; set; }
-    public virtual DateTime? DeletedAt { get; set; }
-}
